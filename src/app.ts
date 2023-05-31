171c8801286fc7367895ea22d7633f943c99e04c
EduardoBarbosa-TI <eduardobb@rede.ulbra.br>
2023-05-31 08:15:29 -0300

    feat: added database connection

---
+++ src/app.ts
@@ -0,0 +1,43 @@
+import express from 'express'
+import routes from './routes/user'
+import 'dotenv/config'
+import { Pool, QueryResult } from 'pg'
+
+class App {
+    public express: express.Application
+    private pool: Pool
+
+    constructor() {
+        this.pool = new Pool({
+            user: process.env.POSTGRES_USER,
+            host: process.env.POSTGRES_DB_HOST,
+            database: process.env.POSTGRES_DB,
+            password: process.env.POSTGRES_PASSWORD,
+            port: 5432,
+        })
+        this.express = express()
+        this.routes()
+        this.database()
+    }
+
+    private middlewares() {
+        this.express.use(express.json())
+    }
+
+    private async database(): Promise<void> {
+        const client = await this.pool.connect();
+    
+        try {
+            console.log('Conexão com o banco de dados estabelecida com sucesso.')
+        } catch(error) {
+            console.error('Erro ao conectar-se ao banco de dados:', error)
+        }
+    }
+
+    private routes(): void {
+        this.express.use(routes)
+    }
+
+}
+
+export default new App().express 
