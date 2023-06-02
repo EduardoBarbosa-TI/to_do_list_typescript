9d4a2686976fe25567871f112e006c32d7cf42dc
EduardoBarbosa-TI <eduardobb@rede.ulbra.br>
2023-06-01 21:38:16 -0300

    fix: added validation for invalid fields

--- src/shared/middlewares/validation.ts
+++ src/shared/middlewares/validation.ts
@@ -1,5 +1,5 @@
-import  { RequestHandler } from 'express'
-import  { ObjectSchema, ValidationError, Maybe, AnyObject } from 'yup'
+import  { RequestHandler} from 'express'
+import  { ObjectSchema, ValidationError, Maybe, AnyObject} from 'yup'
 import { StatusCodes } from 'http-status-codes'
 
 
@@ -19,41 +19,43 @@ export const validation: TValidation = (getAllSchemas) => async (req,res,next) =
   const schemas = getAllSchemas((schema) => schema)
  
   const errorsResult: Record<string,Record<string, string>>= {}
-
-
+  const bodySchema = new ObjectSchema(req.body).fields
+  const errors: Record<string, string> = {};
+  let fieldExists = false;
+ 
   Object.entries(schemas).forEach(([key, schema]) => {
     try{
-     schema.validateSync(req[key as TProperty], { abortEarly: false })
-      
+      schema.validateSync(req[key as TProperty], { abortEarly: false, strict: true }) 
     } catch(err) {
       const yupError = err as ValidationError
-      const errors: Record<string, string>= {}
-  
       yupError.inner.forEach(error => {
+      
+      for (const keybodySchema in bodySchema) {
+        if (Object.prototype.hasOwnProperty.call(bodySchema, keybodySchema)) {
+          const element = keybodySchema;
+           
+          if(!schemas.body?.fields[element]){
+           
+            errors[element] = 'Campo Inexistente'
+          }
+        }
+      }
+        
         if(!error.path) return
         errors[error.path] = error.message
+        
       })
       
       errorsResult[key] = errors
     }
   }) 
 
-  const unexpectedFields= Object.keys(req.body).filter(key => !(key in schemas))
-  
-  if (unexpectedFields.length >  ) {
-    errorsResult['body'] = errorsResult['body'] || {};
-    unexpectedFields.forEach(field => {
-      if(!errorsResult['body'][field]){
-        errorsResult['body'][field] = 'Campo Inexistente';
-      }
-    });
-  }
-  
-  
-  
-  if(Object.entries(errorsResult).length === 0){
-    return next()
-  } else {
-    return res.status(StatusCodes.BAD_REQUEST).json({ errors: errorsResult })
-  }
+ 
+
+
+  if (Object.entries(errorsResult).length === 0) {
+      return next()
+    } else {
+      return res.status(StatusCodes.BAD_REQUEST).json({ errors: errorsResult })
+    }
 }
