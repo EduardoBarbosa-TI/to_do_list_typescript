1ceb05903d09d8d86b450ab12d93751e9fca51e1
EduardoBarbosa-TI <eduardobb@rede.ulbra.br>
2023-06-03 11:41:26 -0300

    feat: added methods in entity user

--- src/entidades/User.ts
+++ src/entidades/User.ts
@@ -1,5 +1,6 @@
-import { Entity, PrimaryGeneratedColumn,CreateDateColumn, Column, BaseEntity } from "typeorm"
+import { Entity, PrimaryGeneratedColumn,CreateDateColumn,UpdateDateColumn, Column, BaseEntity, BeforeUpdate } from "typeorm"
 import { IUser } from "../models"
+import bcrypt from 'bcrypt'
 
 @Entity()
 export class User extends BaseEntity implements IUser {
@@ -9,8 +10,8 @@ export class User extends BaseEntity implements IUser {
     @Column()
     firstName!: string
 
-    @Column()
-    lastName!: string
+    @Column({ nullable: true })
+    lastName?: string
 
     @Column()
     email!: string
@@ -20,4 +21,22 @@ export class User extends BaseEntity implements IUser {
 
     @CreateDateColumn()
     createdAt!: Date
+
+    @BeforeUpdate()
+    @UpdateDateColumn({ nullable: true})
+    updatedAt!: Date | null;
+}
+
+export const getUserByEmail = async (email: string): Promise<User | null> => {
+    const user = await User.findOne({ where: { email }})
+    return user
+}
+
+export const getUserById = async (id: string): Promise<User | null> => {
+    const user = await User.findOne({ where: { id }})
+    return user
+}
+
+export const comparePassword = (password: string,passwordHash: string): Promise<boolean> => {
+    return bcrypt.compare(password, passwordHash)
 }
