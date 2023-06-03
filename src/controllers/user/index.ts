77e7a6ace7e936ae6f83245a7b80284cc69aa3ba
EduardoBarbosa-TI <eduardobb@rede.ulbra.br>
2023-06-03 11:40:47 -0300

    feat: added export of authenticate and getAll in UserController

--- src/controllers/user/index.ts
+++ src/controllers/user/index.ts
@@ -1,5 +1,9 @@
 import * as create from './Create'
+import * as getAll from './GetAll'
+import * as authenticate from './Authenticate'
 
 export const UserController = {
-    ...create
+    ...create,
+    ...authenticate,
+    ...getAll
 }
