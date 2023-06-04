462f8eb30ceb281deb9cdf06800c9e25c2580716
EduardoBarbosa-TI <eduardobb@rede.ulbra.br>
2023-06-04 17:56:32 -0300

    feat: added task getAll method

---
+++ src/controllers/task/GetAll.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from "express"
+import { AppDataSource } from "../../database/data-source"
+import { Task } from "../../entidades"
+import { StatusCodes } from "http-status-codes"
+
+export const getAll = async (req: Request,res: Response) => {
+    try {
+        const task = await AppDataSource.manager.find(Task)
+        return res.status(StatusCodes.OK).json({
+            task: task
+        })
+    } catch (error) {
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+            error: {
+                default: 'Erro ao listar tarefas!'
+            }
+        })
+    }
+}
