4a9bf3cca82750dc16fc98bc7edb7eb998e6bc63
EduardoBarbosa-TI <eduardobb@rede.ulbra.br>
2023-06-04 17:40:38 -0300

    style: removing spaces and changing nomenclature

--- src/controllers/user/Authenticate.ts
+++ src/controllers/user/Authenticate.ts
@@ -1,15 +1,15 @@
 import { Request, Response } from "express"
-import { User, comparePassword, getUserByEmail} from "../../entidades/User"
+import {comparePassword, getUserByEmail} from "../../entidades/User"
 import { StatusCodes } from "http-status-codes"
 import { gerarToken } from "../../auth/auth"
 import * as yup from 'yup'
 import { IUser } from "../../models"
 import { validation } from "../../shared/middlewares"
 
-interface IBodyPropsAuthenticate extends Omit<IUser,'firstName'| 'id' | 'lastName'> {}
+interface IBodyProps extends Omit<IUser,'firstName'| 'id' | 'lastName'> {}
 
 export const authValidation = validation((getSchema) => ({
-    body: getSchema<IBodyPropsAuthenticate>(yup.object().shape({
+    body: getSchema<IBodyProps>(yup.object().shape({
         email: yup.string().email().required(),
         password: yup.string().required()
     }))
@@ -20,14 +20,13 @@ export const authenticate = async (req: Request, res: Response): Promise<Respons
     const {email, password} = req.body
 
     try {
-        const user = await getUserByEmail(email)
 
+        const user = await getUserByEmail(email)
         if(!user){
             return res.status(StatusCodes.NOT_FOUND).send({ message: 'Usuário não encontrado!' })
         }
 
         const passwordMatche = await comparePassword(password,user.password)
-
         if(!passwordMatche){
             return res.status(StatusCodes.UNAUTHORIZED).send({ message: 'Senha incorreta!' })
         }
