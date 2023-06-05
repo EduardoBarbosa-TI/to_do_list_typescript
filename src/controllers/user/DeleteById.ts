23fd31c6fbe4b195f23d1e0d5b05432062fd6bc2
EduardoBarbosa-TI <eduardobb@rede.ulbra.br>
2023-06-05 15:10:59 -0300

    fix: schema taken from user controller

--- src/controllers/user/DeleteById.ts
+++ src/controllers/user/DeleteById.ts
@@ -1,21 +1,9 @@
 import { Request, Response } from 'express'
 import { StatusCodes } from 'http-status-codes'
-import * as yup from 'yup'
-import { validation } from '../../shared/middlewares'
 import { AppDataSource } from '../../database/data-source'
 import { User } from '../../entidades'
 
-interface IParamProps{
-    id?: string
-}
-
-export const deleteByIdValidation = validation(getSchema => ({
-    params: getSchema<IParamProps>(yup.object().shape({
-      id: yup.string().required(),
-    }))
-}))
-
-export const deleteById =  async (req: Request<IParamProps>, res: Response) => {
+export const deleteById =  async (req: Request, res: Response) => {
     if(!req.params.id){
         return res.status(StatusCodes.BAD_REQUEST).json({
             erros: {
