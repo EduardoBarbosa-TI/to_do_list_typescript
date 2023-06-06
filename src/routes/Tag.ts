1c6c96bc099663ca9174d3bf87d591e99fc147d9
EduardoBarbosa-TI <eduardobb@rede.ulbra.br>
2023-06-06 19:12:01 -0300

    feat: added tag routes

---
+++ src/routes/Tag.ts
@@ -0,0 +1,44 @@
+import { Router } from "express";
+import routesTask from "./Task";
+import { authorizeUserByToken } from "../auth";
+import { TagController } from "../controllers";
+import { TagSchema } from "../schemas";
+
+const routesTag = Router()
+
+routesTag.get(
+    '/tags', 
+    authorizeUserByToken,
+    TagController.getAll
+)
+
+routesTag.get(
+    '/tags/:id', 
+    authorizeUserByToken,
+    TagSchema.params,
+    TagController.getbyId
+)
+
+routesTag.post(
+    '/tags', 
+    authorizeUserByToken,
+    TagSchema.bodyCreate,
+    TagController.create
+)
+
+routesTag.put(
+    '/tags/:id', 
+    authorizeUserByToken,
+    TagSchema.params,
+    TagSchema.bodyUpdate,
+    TagController.updateById,
+)
+
+routesTag.delete(
+    '/tags/:id', 
+    authorizeUserByToken,
+    TagSchema.params,
+    TagController.deleteById
+)
+
+export default routesTag
