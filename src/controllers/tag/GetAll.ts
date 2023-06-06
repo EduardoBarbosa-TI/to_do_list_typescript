9a9483540d5adae2c798cf641b9276add64ad12b
EduardoBarbosa-TI <eduardobb@rede.ulbra.br>
2023-06-06 19:09:01 -0300

    feat: added getAll method in tag controller

--- src/controllers/tag/GetAll.ts
+++ src/controllers/tag/GetAll.ts
@@ -5,13 +5,13 @@ import { StatusCodes } from "http-status-codes"
 
 export const getAll = async (req: Request, res: Response) => {
     try {
-        const tag = await AppDataSource.manager.find(Tag)
+        const tag = await AppDataSource.manager.find(Tag, { relations: ['tasks'] })
         return res.status(StatusCodes.OK).json({
             tag: tag
         })
     } catch (error) {
         return res.status(StatusCodes.BAD_REQUEST).json({
-            erros: {
+            errors: {
                 default: 'Erro ao listar tags!'
             }
         })
