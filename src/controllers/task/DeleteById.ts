41b086019956df0c9fff12abfce02a2e9494e592
EduardoBarbosa-TI <eduardobb@rede.ulbra.br>
2023-06-06 20:46:12 -0300

    feat: added deleteById method in task controller

---
+++ src/controllers/task/DeleteById.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from "express";
+import { AppDataSource } from "../../database/data-source";
+import { Task } from "../../entidades";
+import { StatusCodes } from "http-status-codes";
+
+export const deleteById = async (req: Request,res: Response) => {
+   try {
+        await AppDataSource.manager.delete(Task,req.params.id)
+        return res.status(StatusCodes.OK).send()
+   } catch (error) {
+        return res.send(StatusCodes.INTERNAL_SERVER_ERROR).json({
+            errors: {
+                default: 'Erro ao deletetar tarefa!'
+            }
+        })
+   }
+
+}
