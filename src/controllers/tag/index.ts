78322da65e9ec0ce5a8d9c248c270b50ecdabfdd
EduardoBarbosa-TI <eduardobb@rede.ulbra.br>
2023-06-06 19:13:01 -0300

    feat: added tag methods export

--- src/controllers/tag/index.ts
+++ src/controllers/tag/index.ts
@@ -1,15 +1,13 @@
 import * as create from './Create'
 import * as getAll from './GetAll'
-// import * as getById from './GetById'
-// import * as updateById from './UpdateById'
-// import * as deleteById from './DeleteById'
-// import * as authenticate from './Authenticate'
+import * as getById from './GetById'
+import * as updateById from './UpdateById'
+import * as deleteById from './DeleteById'
 
 export const TagController = {
     ...create,
-    // ...authenticate,
     ...getAll,
-    // ...getById,
-    // ...updateById,
-    // ...deleteById,
+    ...getById,
+    ...updateById,
+    ...deleteById,
 }
