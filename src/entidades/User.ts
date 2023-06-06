188ceca98d90943f2b4b413b88f5e882fa2c18ce
EduardoBarbosa-TI <eduardobb@rede.ulbra.br>
2023-06-06 09:03:07 -0300

    feat: added constructor in entity user

--- src/entidades/User.ts
+++ src/entidades/User.ts
@@ -5,20 +5,29 @@ import { Task } from "./Task";
 
 @Entity()
 export class User extends BaseEntity implements IUser {
+
+    constructor(firstname: string, lastName: string, email: string, password: string){
+        super();
+        this.firstName = firstname
+        this.lastName = lastName
+        this.email = email
+        this.password = password
+    }
+
     @PrimaryGeneratedColumn('uuid')
     id!: string;
 
     @Column()
-    firstName!: string
+    firstName: string
 
     @Column({ nullable: true })
     lastName?: string
 
     @Column()
-    email!: string
+    email: string
 
     @Column()
-    password!: string
+    password: string
 
     @CreateDateColumn()
     createdAt!: Date
@@ -29,21 +38,7 @@ export class User extends BaseEntity implements IUser {
 
     @OneToMany(() => Task, (task) => task.user)
     tasks!: Task[]
-}
-
-export const updateById =  async(id_d: string, user: IUser): Promise<UpdateResult | null> => {
-    const updateResult = await User.createQueryBuilder().update(User).set(user).where("id = :id", { id: id_d }).returning('*').execute();
-    return updateResult
-}
-
-export const getUserByEmail = async (email: string): Promise<User | null> => {
-    const user = await User.findOne({ where: { email }})
-    return user
-}
-
-export const getUserById = async (id: string): Promise<User | null> => {
-    const user = await User.findOne({ where: { id }})
-    return user
+    
 }
 
 export const comparePassword = (password: string,passwordHash: string): Promise<boolean> => {
