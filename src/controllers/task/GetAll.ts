057843bb5075fce220097cbef551d56747c703a3
EduardoBarbosa-TI <eduardobb@rede.ulbra.br>
2023-06-06 20:49:15 -0300

    fix: decoupling interfaces and schemas from the getAll method in the task

--- src/controllers/task/GetAll.ts
+++ src/controllers/task/GetAll.ts
@@ -2,25 +2,6 @@ import { Request, Response } from "express"
 import { AppDataSource } from "../../database/data-source"
 import { Task } from "../../entidades"
 import { StatusCodes } from "http-status-codes"
-import { validation } from "../../shared/middlewares";
-import * as yup from 'yup'
-import { ITag } from "../../models";
-
-// interface IQueryProps{
-//     id?: string;
-//     tag?: string;
-//     order?: string;
-//     filter?: string;
-// }
-
-// export const getAllValidation = validation((getSchema) => ({
-//     query: getSchema<IQueryProps>(yup.object().shape({
-//       tag: yup.string().optional(),
-//       order: yup.string().optional(),
-//       id: yup.string().optional(),
-//       filter: yup.string().optional(),
-//     }))
-//   }))
 
 export const getAll = async (req: Request,res: Response) => {
     try {
