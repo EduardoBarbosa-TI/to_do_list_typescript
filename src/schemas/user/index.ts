15cc9ac6269e01ee6646e7342f7ebd200f03891c
EduardoBarbosa-TI <eduardobb@rede.ulbra.br>
2023-06-05 22:33:03 -0300

    feat: added user schema methods export

---
+++ src/schemas/user/index.ts
@@ -0,0 +1,13 @@
+import * as bodyRegister from './BodyRegister'
+import * as bodyLogin from './BodyLogin'
+import * as bodyUpdate from './BodyUpdate'
+import * as params from './Params'
+import * as paramsFilter from './ParamsFilter'
+
+export const UserSchema = {
+    ...bodyRegister,
+    ...bodyLogin,
+    ...bodyUpdate,
+    ...params,
+    ...paramsFilter,
+}
