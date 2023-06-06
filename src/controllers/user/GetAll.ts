5e3a34c416fd992e53a129f7416c7fae76607d2f
EduardoBarbosa-TI <eduardobb@rede.ulbra.br>
2023-06-05 22:41:41 -0300

    fix: decoupling schemas and user interfaces

--- src/controllers/user/GetAll.ts
+++ src/controllers/user/GetAll.ts
@@ -1,29 +1,12 @@
 import { Request, Response } from 'express'
 import { StatusCodes } from 'http-status-codes'
-import * as yup from 'yup'
-import { validation } from '../../shared/middlewares' 
 import { AppDataSource } from '../../database/data-source'
 import { User } from '../../entidades'
-
-interface IQueryProps {
-  id?: string
-  page?: number
-  limit?: number
-  filter?: string
-}
-
-export const getAllValidation = validation((getSchema) => ({
-  query: getSchema<IQueryProps>(yup.object().shape({
-    page: yup.number().optional().moreThan(0),
-    limit: yup.number().optional().moreThan(0),
-    id: yup.string().optional(),
-    filter: yup.string().optional(),
-  }))
-}))
+import { IQueryProps } from '../../schemas/user/ParamsFilter';
 
 export const getAll = async (req: Request<{}, {}, {}, IQueryProps>, res: Response) => {
   try {
-    const users = await AppDataSource.manager.find(User)
+    const users = await AppDataSource.manager.find(User, { relations: ['tasks'] })
     return res.status(StatusCodes.OK).json({
       user: users
     })
