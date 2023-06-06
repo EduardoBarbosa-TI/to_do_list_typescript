1e88e82ed4bafa6fc484ac0f28c07f5d33353ee1
EduardoBarbosa-TI <eduardobb@rede.ulbra.br>
2023-06-06 19:10:39 -0300

    feat: added create method in tag controller

---
+++ src/controllers/tag/UpdateById.ts
@@ -0,0 +1,22 @@
+import { Request, Response } from "express";
+import { AppDataSource } from "../../database/data-source";
+import { Tag } from "../../entidades";
+import { StatusCodes } from "http-status-codes";
+
+export const updateById = async (req: Request,res: Response) => {
+    const { title } = req.body
+
+    try {
+        await AppDataSource.manager.update(Tag,req.params.id,{title: title})
+        return res.status(StatusCodes.CREATED).json({
+            message: 'Tag alterada com sucesso!' 
+        })
+    } catch (error) {
+        console.log(error)
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+            errors: {
+                default: 'Erro ao atualizar tag!'
+            }
+        })
+    }
+}
