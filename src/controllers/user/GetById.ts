fbf04f09f51803cc67052ab273358a7e2a5c56d8
EduardoBarbosa-TI <eduardobb@rede.ulbra.br>
2023-06-06 20:52:26 -0300

    style: reducing spaces

--- src/controllers/user/GetById.ts
+++ src/controllers/user/GetById.ts
@@ -1,17 +1,14 @@
 import { Request, Response } from 'express'
 import { StatusCodes } from 'http-status-codes'
-import { IProps } from '../../schemas/user/Params'
+import { IProps } from '../../schemas/Params'
 import { User } from '../../entidades'
 import { AppDataSource } from '../../database/data-source'
 
 export const getById = async (req: Request<IProps>,  res: Response) => {
     const id = req.params.id
-
     try {
         const user = await AppDataSource.manager.findOne(User, { where: { id } });
-        return res.status(StatusCodes.OK).json({
-            user
-        })
+        return res.status(StatusCodes.OK).json({ user })
     } catch (error) {
         return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
             error:{
