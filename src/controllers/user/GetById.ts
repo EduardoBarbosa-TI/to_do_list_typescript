55e3638eb6eb9f87c1c9a2cc02e0c5659f8de705
EduardoBarbosa-TI <eduardobb@rede.ulbra.br>
2023-06-06 09:13:19 -0300

    fix: withdrawal of unnecessary validation

--- src/controllers/user/GetById.ts
+++ src/controllers/user/GetById.ts
@@ -1,24 +1,22 @@
 import { Request, Response } from 'express'
 import { StatusCodes } from 'http-status-codes'
-import { getUserById } from '../../entidades'
 import { IProps } from '../../schemas/user/Params'
+import { User } from '../../entidades'
+import { AppDataSource } from '../../database/data-source'
 
 export const getById = async (req: Request<IProps>,  res: Response) => {
-    if(!req.params){
-        return res.status(StatusCodes.BAD_REQUEST).json({
-            errors:{
-                default: 'O parâmetro id precisa ser informado'
-            }
-        })
-    }
+    const id = req.params.id
+
     try {
-        const result =  await getUserById(String(req.params.id))
-        return res.status(StatusCodes.OK).json(result)
+        const user = await AppDataSource.manager.findOne(User, { where: { id } });
+        return res.status(StatusCodes.OK).json({
+            user
+        })
     } catch (error) {
-         return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
             error:{
                 default: 'uuid inválido!'
             }
         })
-    }  
+    }
 }
