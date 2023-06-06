0e45fbbeaf1ebd5074e25e70fd9af9f308ce3cab
EduardoBarbosa-TI <eduardobb@rede.ulbra.br>
2023-06-05 22:41:33 -0300

    fix: decoupling schemas and user interfaces

--- src/controllers/user/Create.ts
+++ src/controllers/user/Create.ts
@@ -1,22 +1,8 @@
 import { Request, Response } from "express"
-import { IUser } from "../../models/User"
-import { validation } from "../../shared/middlewares"
-import * as yup from 'yup'
 import bcrypt from 'bcrypt';
 import { User } from "../../entidades/User"
 import { AppDataSource } from "../../database/data-source"
 import { StatusCodes } from "http-status-codes"
-
-interface IBodyProps extends Omit<IUser, 'id'> {}
-
-export const createValidation = validation((getSchema) => ({
-    body: getSchema<IBodyProps>(yup.object().shape({
-        firstName: yup.string().required().min(3).max(150),
-        lastName: yup.string().optional().min(3).max(150),
-        email: yup.string().email().required(),
-        password: yup.string().required()
-    }))
-}))
 
 export const create = async (req: Request,res: Response)=> {
     const {password,firstName,lastName,email} = req.body
