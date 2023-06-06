bb243031db0951dcbe565755bd5821cc64834611
EduardoBarbosa-TI <eduardobb@rede.ulbra.br>
2023-06-06 19:06:21 -0300

    refactor: removing schema and sending attributes by constructor

--- src/controllers/tag/Create.ts
+++ src/controllers/tag/Create.ts
@@ -1,25 +1,12 @@
 import { Request, Response } from "express";
-import { validation } from "../../shared/middlewares";
-import { ITag } from "../../models";
-import * as yup from 'yup'
 import { Tag } from "../../entidades";
 import { StatusCodes } from "http-status-codes";
  
-interface IBodyProps extends Omit<ITag,'id'> {}
-
-export const createValidation = validation(getSchema => ({
-    body: getSchema<IBodyProps>(yup.object().shape({
-        titulo: yup.string().required().min(5).max(20)
-    }))
-}))
-
 export const create = async (req: Request,res: Response) => {
-    const { titulo } = req.body  
-
-    const tag = new Tag
-    tag.titulo = titulo
+    const { title } = req.body  
 
     try {
+        const tag = new Tag(title)
         await tag.save()
         return res.status(StatusCodes.CREATED).json({
             message: 'Tag adicionado com sucesso!, ' + tag.id
