851c79b926a275684eb95810d4a0d6108569765e
EduardoBarbosa-TI <eduardobb@rede.ulbra.br>
2023-06-06 09:05:31 -0300

    fix: validation withdrawn

--- src/controllers/user/DeleteById.ts
+++ src/controllers/user/DeleteById.ts
@@ -4,16 +4,9 @@ import { AppDataSource } from '../../database/data-source'
 import { User } from '../../entidades'
 
 export const deleteById =  async (req: Request, res: Response) => {
-    if(!req.params.id){
-        return res.status(StatusCodes.BAD_REQUEST).json({
-            erros: {
-                default: 'O parâmetro "id" precisa ser informado'
-            }
-        })
-    }
     try {
         await AppDataSource.manager.delete(User,req.params.id)
-        return res.status(StatusCodes.NO_CONTENT).send()
+        return res.status(StatusCodes.OK).send()
     } catch (error) {
         return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
             error: {
