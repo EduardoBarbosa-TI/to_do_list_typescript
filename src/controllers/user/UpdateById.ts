dc74575e7bae61cf38872baa805869ef5ce19045
EduardoBarbosa-TI <eduardobb@rede.ulbra.br>
2023-06-06 20:53:36 -0300

    fix: changing import of IProps

--- src/controllers/user/UpdateById.ts
+++ src/controllers/user/UpdateById.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from 'express'
 import { StatusCodes } from 'http-status-codes'
 import { IBodyProps } from '../../schemas/user/BodyUpdate';
-import { IProps } from '../../schemas/user/Params';
+import { IProps } from '../../schemas/Params';
 import { AppDataSource } from '../../database/data-source';
 import { User } from '../../entidades';
 import bcrypt from 'bcrypt';
