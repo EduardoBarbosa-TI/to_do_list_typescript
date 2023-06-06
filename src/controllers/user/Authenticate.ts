34345b2a28573144859fdbe4ae8bc90777066717
EduardoBarbosa-TI <eduardobb@rede.ulbra.br>
2023-06-06 09:12:04 -0300

    refactor: added refactoring in authenticate user

--- src/controllers/user/Authenticate.ts
+++ src/controllers/user/Authenticate.ts
@@ -1,34 +1,27 @@
 import { Request, Response } from "express"
-import {comparePassword, getUserByEmail} from "../../entidades/User"
+import {User, comparePassword} from "../../entidades/User"
 import { StatusCodes } from "http-status-codes"
 import { gerarToken } from "../../auth/auth"
 import { IUser } from "../../models"
+import { AppDataSource } from "../../database/data-source"
+import { FindOneOptions } from "typeorm"
+import { string } from "yup"
 
 export const authenticate = async (req: Request, res: Response): Promise<Response> => 
 {
     const {email, password} = req.body
+    const user = await AppDataSource.manager.findOne(User, { where: { email } })
 
-    try {
-
-        const user = await getUserByEmail(email)
-        if(!user){
+    if(!user){
             return res.status(StatusCodes.NOT_FOUND).send({ message: 'Usuário não encontrado!' })
-        }
+    }
 
-        const passwordMatche = await comparePassword(password,user.password)
-        if(!passwordMatche) return res.status(StatusCodes.UNAUTHORIZED).send({ message: 'Senha incorreta!' })
-        
-        return res.json({
-            user: user,
-            token: gerarToken(user as IUser)
-        })
-        
-    } catch (error) {
-        console.log(error)
-        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-            errors: {
-            default: error
-        }
-    })}
+    const passwordMatche = await comparePassword(password,user.password)
+    if(!passwordMatche) return res.status(StatusCodes.UNAUTHORIZED).send({ message: 'Senha incorreta!' })
+
+    return res.json({
+        user: user,
+        token: gerarToken(user as IUser)
+    })
 }
 
