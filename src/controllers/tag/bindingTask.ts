aade32e1613d5092c050e0a354e593ac9c03ebe1
EduardoBarbosa-TI <eduardobb@rede.ulbra.br>
2023-06-07 09:58:11 -0300

    feat: added binding with task

---
+++ src/controllers/tag/bindingTask.ts
@@ -0,0 +1,30 @@
+import { Request, Response } from "express";
+import { Tag, Task } from "../../entidades";
+import { AppDataSource } from "../../database/data-source";
+import { StatusCodes } from "http-status-codes";
+
+export const bindingTask = async (req: Request, res:Response) => {
+    const { title } = req.body
+
+    const tag =  new Tag(title)
+    await tag.save()
+    try {
+        const task = await AppDataSource.manager.findOne(Task, { where: { id: req.params.id }, relations: ['tags'] })
+        if(!task){
+            throw new Error('Id inválido!')
+        }
+        task.tags.push(tag)
+        await task.save()
+
+        return res.status(StatusCodes.CREATED).json({
+            message: 'Tag adicionada com sucesso e vinculada com a task!, idTask: ' + task.id
+        })
+    } catch (error) {
+        console.log(error)
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+            error: {
+                default: 'Erro ao adicionar a tag e vincular com a task'
+            }
+        })
+    }
+}
