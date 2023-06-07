b4bca0657dc7187ab07bc0cdaf44af30c0404009
EduardoBarbosa-TI <eduardobb@rede.ulbra.br>
2023-06-07 20:58:19 -0300

    feat: added getAllFilter method on task entity

--- src/controllers/task/GetAll.ts
+++ src/controllers/task/GetAll.ts
@@ -1,14 +1,12 @@
 import { Request, Response } from "express"
 import { Task} from "../../entidades"
 import { StatusCodes } from "http-status-codes"
-import { AppDataSource } from "../../database/data-source"
+import { IQueryProps } from "../../schemas/task/QueryFilter"
 
-export const getAll = async (req: Request,res: Response) => {
+export const getAll = async (req: Request<never,never,never,IQueryProps>,res: Response) => {
     try {
-        const task = await AppDataSource.manager.find(Task, { relations: ['user','tags'] })
-        return res.status(StatusCodes.OK).json({
-            task: task
-        })
+        const task = await Task.getAllFilter(req.query)
+        return res.status(StatusCodes.OK).json({ task })
     } catch (error) {
         return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({   
             error: {
