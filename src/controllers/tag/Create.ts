0c2b042a465551d6fa806e981130803d505d49e6
EduardoBarbosa-TI <eduardobb@rede.ulbra.br>
2023-06-07 16:58:52 -0300

    refactor: removing empty spaces and modifying the positioning

--- src/controllers/tag/Create.ts
+++ src/controllers/tag/Create.ts
@@ -4,9 +4,9 @@ import { StatusCodes } from "http-status-codes";
  
 export const create = async (req: Request,res: Response) => {
     const { title } = req.body  
+    const tag = new Tag(title)
 
     try {
-        const tag = new Tag(title)
         await tag.save()
         return res.status(StatusCodes.CREATED).json({
             message: 'Tag adicionado com sucesso!, ' + tag.id
