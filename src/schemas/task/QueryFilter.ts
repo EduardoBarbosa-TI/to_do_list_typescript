6466ef088481c8f13a571254285c546668b90bbd
EduardoBarbosa-TI <eduardobb@rede.ulbra.br>
2023-06-07 17:06:19 -0300

    fix: changing order typing

--- src/schemas/task/QueryFilter.ts
+++ src/schemas/task/QueryFilter.ts
@@ -4,14 +4,14 @@ import * as yup  from 'yup'
 export interface IQueryProps{
     id?: string;
     tag?: string;
-    order?: string;
+    order?: "asc" | "desc";
     filter?: string;
 }
 
 export const queryFilter = validation((getSchema) => ({
     query: getSchema<IQueryProps>(yup.object().shape({
       tag: yup.string().optional(),
-      order: yup.string().optional(),
+      order: yup.mixed<"asc" | "desc">().oneOf(["asc", "desc"]).optional(),
       id: yup.string().optional(),
       filter: yup.string().optional(),
     }))
