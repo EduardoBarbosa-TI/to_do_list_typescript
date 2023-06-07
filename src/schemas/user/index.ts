c90078adca6c2ee58ddc3fc55e2badcf8d9489ac
EduardoBarbosa-TI <eduardobb@rede.ulbra.br>
2023-06-06 21:05:15 -0300

    style: import path change

--- src/schemas/user/index.ts
+++ src/schemas/user/index.ts
@@ -1,7 +1,7 @@
 import * as bodyRegister from './BodyRegister'
 import * as bodyLogin from './BodyLogin'
 import * as bodyUpdate from './BodyUpdate'
-import * as params from './Params'
+import * as params from '../Params'
 import * as paramsFilter from './ParamsFilter'
 
 export const UserSchema = {
