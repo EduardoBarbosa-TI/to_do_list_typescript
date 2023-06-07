24bbe69e77940e76ac144f41cc1bad352290f56b
EduardoBarbosa-TI <eduardobb@rede.ulbra.br>
2023-06-07 17:00:29 -0300

    refactor: removing empty spaces and consoles

--- src/controllers/tag/UpdateById.ts
+++ src/controllers/tag/UpdateById.ts
@@ -5,14 +5,12 @@ import { StatusCodes } from "http-status-codes";
 
 export const updateById = async (req: Request,res: Response) => {
     const { title } = req.body
-
     try {
         await AppDataSource.manager.update(Tag,req.params.id,{title: title})
         return res.status(StatusCodes.CREATED).json({
             message: 'Tag alterada com sucesso!' 
         })
     } catch (error) {
-        console.log(error)
         return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
             errors: {
                 default: 'Erro ao atualizar tag!'
