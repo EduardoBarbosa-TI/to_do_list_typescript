b90f9384dd25eaa709f78817b75902a754116b19
EduardoBarbosa-TI <eduardobb@rede.ulbra.br>
2023-06-07 17:00:06 -0300

    refactor: modifying return

--- src/controllers/tag/GetById.ts
+++ src/controllers/tag/GetById.ts
@@ -6,9 +6,7 @@ import { StatusCodes } from "http-status-codes"
 export const getbyId = async (req: Request, res: Response) => {
     try {
         const tag = await AppDataSource.manager.findOne(Tag, {where: {id: req.params.id}, relations: ['tasks'] })
-        return res.status(StatusCodes.OK).json({
-            tag: tag
-        })
+        return res.status(StatusCodes.OK).json({ tag })
     } catch (error) {
         return res.status(StatusCodes.BAD_REQUEST).json({
             errors: {
