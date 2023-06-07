b2267e6f63a4a324a16534b80569124eed7cefda
EduardoBarbosa-TI <eduardobb@rede.ulbra.br>
2023-06-07 10:13:30 -0300

    feat: added export bidingTag method

--- src/controllers/tag/index.ts
+++ src/controllers/tag/index.ts
@@ -1,11 +1,14 @@
 import * as create from './Create'
 import * as getAll from './GetAll'
+import * as bidingTag from './bindingTask'
 import * as getById from './GetById'
 import * as updateById from './UpdateById'
 import * as deleteById from './DeleteById'
 
+
 export const TagController = {
     ...create,
+    ...bidingTag,
     ...getAll,
     ...getById,
     ...updateById,
