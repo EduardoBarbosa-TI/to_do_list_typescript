965042e552b67ab070892103e7d62b9764a3e249
EduardoBarbosa-TI <eduardobb@rede.ulbra.br>
2023-06-07 10:20:30 -0300

    fix: added array of tags in constructor

--- src/controllers/task/UpdateById.ts
+++ src/controllers/task/UpdateById.ts
@@ -7,7 +7,7 @@ export const updateById  = async (req: Request,res: Response) => {
     const { title , description } = req.body
 
     try {
-        const newTask = new Task(title,description)
+        const newTask = new Task(title,description,[])
 
         await AppDataSource.manager.update(Task,req.params.id,newTask)
 
