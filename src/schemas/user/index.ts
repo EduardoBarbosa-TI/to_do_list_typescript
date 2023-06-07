294f60ef6451aa1325d414ddd0d2bde3cac8c70b
EduardoBarbosa-TI <eduardobb@rede.ulbra.br>
2023-06-07 17:06:54 -0300

    fix: removed method IParamsID

--- src/schemas/user/index.ts
+++ src/schemas/user/index.ts
@@ -2,12 +2,11 @@ import * as bodyRegister from './BodyRegister'
 import * as bodyLogin from './BodyLogin'
 import * as bodyUpdate from './BodyUpdate'
 import * as params from '../Params'
-import * as paramsFilter from './ParamsFilter'
+
 
 export const UserSchema = {
     ...bodyRegister,
     ...bodyLogin,
     ...bodyUpdate,
     ...params,
-    ...paramsFilter,
 }
