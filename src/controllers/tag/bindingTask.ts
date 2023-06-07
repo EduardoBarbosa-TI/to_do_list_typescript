a85765c15173fd7726faf31a16d29ab3eef2dc12
EduardoBarbosa-TI <eduardobb@rede.ulbra.br>
2023-06-07 16:58:06 -0300

    refactor: removing empty spaces and consoles

--- src/controllers/tag/bindingTask.ts
+++ src/controllers/tag/bindingTask.ts
@@ -5,14 +5,12 @@ import { StatusCodes } from "http-status-codes";
 
 export const bindingTask = async (req: Request, res:Response) => {
     const { title } = req.body
-
     const tag =  new Tag(title)
     await tag.save()
     try {
         const task = await AppDataSource.manager.findOne(Task, { where: { id: req.params.id }, relations: ['tags'] })
-        if(!task){
-            throw new Error('Id inválido!')
-        }
+        if(!task){throw new Error('Id inválido!')}
+
         task.tags.push(tag)
         await task.save()
 
@@ -20,7 +18,6 @@ export const bindingTask = async (req: Request, res:Response) => {
             message: 'Tag adicionada com sucesso e vinculada com a task!, idTask: ' + task.id
         })
     } catch (error) {
-        console.log(error)
         return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
             error: {
                 default: 'Erro ao adicionar a tag e vincular com a task'
