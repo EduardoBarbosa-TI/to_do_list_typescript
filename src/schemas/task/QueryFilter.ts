e2d42bb00dd2bb5cf7e51b8f50a680113d7f3110
EduardoBarbosa-TI <eduardobb@rede.ulbra.br>
2023-06-07 21:06:52 -0300

    fix: added uppercase order desc and asc in queryFilter interface

--- src/schemas/task/QueryFilter.ts
+++ src/schemas/task/QueryFilter.ts
@@ -2,16 +2,18 @@ import { validation } from "../../shared/middlewares"
 import * as yup  from 'yup'
 
 export interface IQueryProps{
-    id?: string;
-    tag?: string;
-    order?: "asc" | "desc";
-    filter?: string;
+    id?: string
+    title?: string
+    tag?: string
+    order?: "ASC" | "DESC" | 'asc' | 'desc'
+    filter?: string
 }
 
 export const queryFilter = validation((getSchema) => ({
     query: getSchema<IQueryProps>(yup.object().shape({
       tag: yup.string().optional(),
-      order: yup.mixed<"asc" | "desc">().oneOf(["asc", "desc"]).optional(),
+      order: yup.mixed<"ASC" | "DESC" | 'asc' | 'desc'>().oneOf(["ASC", "DESC","asc","desc"]).optional(),
+      title: yup.string().optional(),
       id: yup.string().optional(),
       filter: yup.string().optional(),
     }))
