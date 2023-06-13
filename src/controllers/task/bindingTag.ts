f05f8ad6e2939fb99db77e8f6ee2c71930ecbc7b
EduardoBarbosa-TI <eduardobb@rede.ulbra.br>
2023-06-13 13:03:39 -0300

    fix: added repository task in controller bindingTag

--- src/controllers/task/bindingTag.ts
+++ src/controllers/task/bindingTag.ts
@@ -1,26 +1,28 @@
 import { Request, Response } from "express";
-import { Tag, Task, User } from "../../entidades";
-import { AppDataSource } from "../../database/data-source";
+import {Task} from "../../entidades";
+
 import { StatusCodes } from "http-status-codes";
+import { tagRepository } from "../../repositories/TagRepository";
+import { userRepository } from "../../repositories/UserRepository";
+import { taskRepository } from "../../repositories/TaskRepository";
 
 export const bindingTag = async (req: Request, res:Response) => {
     const {title, description} = req.body
-    const task =  Task.createWithTask(title,description,[])  
+    const task =  new Task(title,description,[])  
     const id = String(req.headers['id-access-token'])
     
     try {
-        const tag = await AppDataSource.manager.findOne(Tag, { where: { id: req.params.id }})
-        const user = await AppDataSource.manager.findOne(User, { where: { id: id} })
+        const tag = await tagRepository.findOne({ where: { id: req.params.id }})
+        const user = await userRepository.findOne({ where: { id: id} })
 
         if(!user){ throw new Error('Usuário não encontrado!')
         }else if(!tag){ throw new Error('Id inválido!') }
 
         task.tags.push(tag)
-        await task.save()
-        
         task.user = user 
-        await task.save()
 
+        await taskRepository.save(task)
+    
         return res.status(StatusCodes.CREATED).json({
             message: 'Tag adicionada com sucesso e vinculada com a task!, idTask: ' + task.id
         })
