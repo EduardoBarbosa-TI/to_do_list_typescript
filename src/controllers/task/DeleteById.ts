abe8883fc176c8c0118e3eeee67cf78b88de1320
EduardoBarbosa-TI <eduardobb@rede.ulbra.br>
2023-06-13 13:04:40 -0300

    fix: added repository task in controller deleteById

--- src/controllers/task/DeleteById.ts
+++ src/controllers/task/DeleteById.ts
@@ -1,11 +1,10 @@
 import { Request, Response } from "express";
-import { AppDataSource } from "../../database/data-source";
-import { Task } from "../../entidades";
 import { StatusCodes } from "http-status-codes";
+import { taskRepository } from "../../repositories/TaskRepository";
 
 export const deleteById = async (req: Request,res: Response) => {
    try {
-        await AppDataSource.manager.delete(Task,req.params.id)
+        await taskRepository.delete(req.params.id)
         return res.status(StatusCodes.OK).send()
    } catch (error) {
         return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
