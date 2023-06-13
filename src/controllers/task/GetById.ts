b18aa49ccd93f77121d0211846942b3ba1a9ca1a
EduardoBarbosa-TI <eduardobb@rede.ulbra.br>
2023-06-13 13:03:07 -0300

    fix: added repository task in controller getById

--- src/controllers/task/GetById.ts
+++ src/controllers/task/GetById.ts
@@ -1,11 +1,12 @@
 import { Request, Response } from "express"
-import { AppDataSource } from "../../database/data-source"
+import { AppDataSource } from "../../connection/data-source"
 import { Task } from "../../entidades"
 import { StatusCodes } from "http-status-codes"
+import { taskRepository } from "../../repositories/TaskRepository"
 
 export const getById = async (req: Request,res: Response) => {
     try {
-        const task = await AppDataSource.manager.findOne(Task,{ where: { id: req.params.id }, relations: ['user','tags'] })
+        const task = await taskRepository.findOne({ where: { id: req.params.id }, relations: ['user','tags'] })
         return res.status(StatusCodes.OK).json({
             task: task
         })
