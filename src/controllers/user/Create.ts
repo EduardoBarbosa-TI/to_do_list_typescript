4f78ce504157f24d9cf544d40117ffdbf4d74452
EduardoBarbosa-TI <eduardobb@rede.ulbra.br>
2023-06-16 09:26:57 -0300

    refactor: rename import path and change password hash

--- src/controllers/user/Create.ts
+++ src/controllers/user/Create.ts
@@ -1,13 +1,12 @@
 import { Request, Response } from "express"
 import bcrypt from 'bcrypt';
-import { User } from "../../entidades/User"
-import { AppDataSource } from "../../connection/data-source"
+import { User } from "../../entities/User"
 import { StatusCodes } from "http-status-codes"
 import { userRepository } from "../../repositories/UserRepository";
 
 export const create = async (req: Request,res: Response)=> {
     const {password,firstName,lastName,email} = req.body 
-    const hashedPassword =  await bcrypt.hash(password,15)
+    const hashedPassword =  await bcrypt.hash(password,10)
     const user = new User(firstName,lastName,email,hashedPassword)
 
     try {
